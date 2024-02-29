e31ecebc0356e08f08b4b810a045139e3ee9d57d
akshita <akshitasemwal@gmail.com>
2024-02-29 15:45:51 +0530

    Updated: projects display UI

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -33,17 +33,22 @@ const Work = () => {
                   listings of their choice and search for desired listings using
                   the search functionality.
                 </p>
-                <a
-                  href="https://github.com/akshitasemwal/Estate-Ecommerce"
-                  target="new"
-                  className=""
-                >
-                  <button className="pink-600 p-2 border-2 border-[#dce2f3]">
-                    Code
-                  </button>
-                </a>
+                <div className="p-3">
+                  <a
+                    href="https://github.com/akshitasemwal/Estate-Ecommerce"
+                    target="new"
+                  >
+                    <button className="pink-600 py-2.5 px-5 border-2 border-[#dce2f3]">
+                      Code
+                    </button>
+                  </a>
 
-                <button>Live</button>
+                  <a href="https://estateecommerce.onrender.com/" target="new">
+                    <button className="py-2.5 px-6 border-2 border-pink-600 bg-pink-600 ml-6">
+                      Live
+                    </button>
+                  </a>
+                </div>
               </div>
             </div>
             <div className="items-center">
@@ -63,36 +68,71 @@ const Work = () => {
                 className="shadow-lg shadow-[#040c16] group container rounded mx-auto content-div p-6 sm:min-w-fit my-bottom-7 h-60"
               ></div>
             </div>
-            <div className="text-left text-3xl font-bold text-[#dce2f3]">
-              <p>BlogSite</p>
+            <div className="">
+              <p className="text-4xl py-2 text-left font-bold text-[#dce2f3]">
+                BlogSite
+              </p>
+              <p>
+                A fullstack web application with user authentication and
+                verification. Streamlined retrieval of blog posts and apply CRUD
+                operations for dynamic blog entries, creating, updating and
+                deleting individual blogs by use of RESTful APIs.
+              </p>
+
+              <div className="p-3">
+                <a
+                  href="https://github.com/akshitasemwal/BlogSite"
+                  target="new"
+                >
+                  <button className="pink-600 py-2.5 px-5 border-2 border-[#dce2f3]">
+                    Code
+                  </button>
+                </a>
+
+                {/* <a href="https://estateecommerce.onrender.com/" target="new">
+                  <button className="py-2.5 px-6 border-2 border-pink-600 bg-pink-600 ml-6">
+                    Live
+                  </button>
+                </a> */}
+              </div>
             </div>
           </div>
         </div>
 
         <div className="flex flex-col justify-center items-center w-full">
           <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-6 p-8">
-            <div className="text-left text-3xl font-bold text-[#dce2f3]">
-              <p>Estate Ecommerce</p>
-            </div>
-            <div className="items-center">
-              <div
-                style={{ backgroundImage: `url(${StudentDashboard})` }}
-                className="shadow-lg shadow-[#040c16] group container rounded mx-auto content-div p-6 sm:min-w-fit my-bottom-7 h-60"
-              ></div>
-            </div>
-          </div>
-        </div>
+            <div>
+              <p className="text-4xl py-2 text-left font-bold text-[#dce2f3]">
+                Student Dashboard
+              </p>
+              <p>
+                Developed a web application to manage student data using Angular
+                and JSON database. Designed the frontend using Angular and built
+                the backend using JSON database to store and retrieve student
+                data.
+              </p>
+              <div className="p-3">
+                <a
+                  href="https://github.com/akshitasemwal/StudentDashboard"
+                  target="new"
+                >
+                  <button className="pink-600 py-2.5 px-5 border-2 border-[#dce2f3]">
+                    Code
+                  </button>
+                </a>
 
-        <div className="flex flex-col justify-center items-center w-full">
-          <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-6 p-8">
+                {/* <a href="https://estateecommerce.onrender.com/" target="new">
+                  <button className="py-2.5 px-6 border-2 border-pink-600 bg-pink-600 ml-6">
+                    Live
+                  </button>
+                </a> */}
+              </div>
+            </div>
             <div className="items-center">
               <div
                 style={{ backgroundImage: `url(${StudentDashboard})` }}
                 className="shadow-lg shadow-[#040c16] group container rounded mx-auto content-div p-6 sm:min-w-fit my-bottom-7 h-60"
               ></div>
-            </div>
-            <div className="text-left text-3xl font-bold text-[#dce2f3]">
-              <p>Estate Ecommerce</p>
             </div>
           </div>
         </div>
